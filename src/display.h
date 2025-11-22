5964ee1badf4bcdc6eb5502c67181325fcb54f43
ailsonlcosta <engenharia.software@grupomulti.com.br>
2025-11-22 18:26:29 -0300

    first commit

---
+++ src/display.h
@@ -0,0 +1,14 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+
+#define SSD1306_I2C_SDA 14
+#define SSD1306_I2C_SCL 15
+#define SSD1306_I2C_CLOCK 400
+
+void display_init(void);
+void display_status_msg(const char status_msg[]);
+void display_multiline(const char* line1, const char* line2);
+
+#endif
